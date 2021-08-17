1ddf7a1a0a9aa98dc2fc79c68f9ea4a2470a430b
idohiro <idohiro0509@gmail.com>
2021-08-17 11:55:05 +0000

    フォーム追加について

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #devise_for :users
+
+     devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
     root to: 'homes#top'
 end
